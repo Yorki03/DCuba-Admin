e2d2a25cf1142099ce2ce2f15218cc84870cf602
Yorki03 <110152632+Yorki03@users.noreply.github.com>
2023-08-20 16:02:58 +0200

    inicio del proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dcuba-admin',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
